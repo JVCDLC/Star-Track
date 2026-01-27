d098fcec4d8477706ca5d82637d2e4159d6b17ed
JosserandPierre <59586918+JosserandPierre@users.noreply.github.com>
2026-01-27 17:44:34 -0500

    Add motor control request function for ESP-32
    Introduced Motor enum and send_request_motor function to send motor position requests to ESP-32 and handle acknowledgments. Updated main loop to demonstrate motor control requests and improved serial communication handling.

--- ESP-32 code/Python_part/main.py
+++ ESP-32 code/Python_part/main.py
@@ -1,11 +1,57 @@
 import serial
 import time
-# pour linux(Raspberry pi)
+from enum import Enum
+
+
+class Motor(Enum):
+    DECLIMATION = 1
+    RIGHT_ASCENSION = 2
+    FOCUS = 3
+
+### send motor and angle to esp-32.  wait for the answer back from the esp32
+#   REQUEST,MOTOR,180.1234\n
+def send_request_motor(ser, motor: Motor, position: float, timeout=1.0) -> bool:
+    message = f"REQUEST,{motor.name},{position}\n"
+    ser.write(message.encode())
+    print("TX:", message.strip())
+
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        if ser.in_waiting:
+            reply = ser.readline().decode().strip()
+            print("RX:", reply)
+
+            # Succes if we ave a answer
+            if reply.startswith("ACK"):
+                return True
+            else:
+                return False
+
+    # timeout took too much time to have answer, should implement: send a new message if no answer
+    print("RX: timeout")
+    return False
+
+
+# For Linux(Raspberry pi)
 #ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 ser = serial.Serial('COM8', 115200, timeout=1)
-time.sleep(2)  # important pour laisser l'ESP32 reset
+time.sleep(2)  # wait for esp-32 to reset
 
-ser.write(b'hello esp32\n')
+try:
+    while True:
+        # 120.15 degrees
+        send_request_motor(ser, Motor.RIGHT_ASCENSION, 120.15)
+        time.sleep(5)
 
-reply = ser.readline().decode().strip()
-print(reply)
+        # 0 degres
+        send_request_motor(ser, Motor.RIGHT_ASCENSION, 0.0)
+        time.sleep(5)
+
+# stop if user touch a key
+except KeyboardInterrupt:
+    print("stop")
+
+finally:
+    ser.close()
+
+
