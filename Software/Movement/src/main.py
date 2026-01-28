17e39015ac59221e5e2d369b4250dd08bc9616bb
Antoine Desgranges <desa1135@usherbrooke.ca>
2026-01-28 11:30:17 -0500

    Correction of Ha Dec axe for localisation

--- Software/Movement/src/main.py
+++ Software/Movement/src/main.py
@@ -10,6 +10,7 @@
 # Import necessary modules
 
 #from movement_controller import MovementController
+from numpy import angle
 from skyfield.api import load, Star, wgs84
 from skyfield.data import hipparcos
 
@@ -31,59 +32,55 @@ polaris= Star.from_dataframe(stars.loc[11767]) #refrence star Polaris
 latitude = 45.365434 #  N 45° 21' 55.563''
 longitude = -71.939477 # W 71° 56' 22.115''
 observer = earth + wgs84.latlon(latitude, longitude)
+temperature = 15.0 #degrees Celsius
+pressure = 1005.0 #mbar
 #-------------------------------------------------------------------------------------------#
-
-class motorRotation:
+#Classes
+        
+class CoordinateOfCelestialBody:   
     """
-    Class to store motor rotation information
+    Class to store celestial body coordinate information
     Attributes:
-        currentPosition : Current position of the motor in degrees
-        targetPosition : Target position of the motor in degrees
+        ha : Hour Angle of the celestial body
+        dec : Declination of the celestial body
 
     Methods:
-        __init__(self, currentPosition=0.0, targetPosition=0.0):
-            Initializes the motorRotation object with current and target positions.
-        moveToTarget(self):
-            Simulates moving the motor to the target position with a PID.
-        updateTarget(self, newTarget):
-            Updates the target position of the motor.
-        predictTimeToTarget(self, maxSpeed):
-            Predicts the time required to reach the target position based on speed.
+        __init__(self, ha=0.0, dec=0.0):
+            Initializes the coordinateOfCelestialBody object with hour angle and declination.
+        updateCoordinates(self, newHa, newDec):
+            Updates the hour angle and declination of the celestial body.
+        
     """
-    def __init__(self, currentPosition=0.0, targetPosition=0.0):
-        self.currentPosition = currentPosition
-        self.targetPosition = targetPosition
+    def __init__(self,body, bodyName = 'name', ha=0.0, dec=0.0):
+        self.bodyName = bodyName
+        self.body = body
+        self.updateCoordinates()
 
-    def moveToTarget(self):
+    def updateCoordinates(self):
         """
-        Simulates moving the motor to the target position with a PID.
-        """
-        # Placeholder for PID control logic
-        self.currentPosition = self.targetPosition
-        print(f"Motor moved to target position: {self.targetPosition} degrees")
-
-    def updateTarget(self, newTarget):
-        """
-        Updates the target position of the motor.
-        Inputs: newTarget : New target position in degrees
+        Updates the hour angle and declination of the celestial body.
+        Inputs: newHa : New hour angle in degrees
+                newDec : New declination in degrees
         Outputs: None
         """
-        self.targetPosition = newTarget
+        ha , dec = print_HaDec(self.body,self.bodyName)
+        self.ha = ha
+        self.dec = dec
         return
-        
-
-    def predictTimeToTarget(self, maxSpeed):
+    def convertDegreeToMotorPositions(self): #jp a dit pas nécessaire
         """
-        Predicts the time required to reach the target position based on speed.
-        Inputs: speed : Speed of the motor in degrees per second
-        Outputs: time : Time in seconds to reach the target position
+            Converts the hour angle and declination in degrees to motor positions.
+            Inputs: None
+            Outputs: motorPosition : Motor position in steps
         """
-        distance = abs(self.targetPosition - self.currentPosition)
-        if maxSpeed <= 0:
-            raise ValueError("Speed must be greater than zero")
-        time = distance / maxSpeed #in degrees per second
-        return time
+        gearRatio = 10 # Gear ratio
+        motorEncoderStepsPerRevolution = 2050 # Motor encoder steps per revolution
+        motorPosition = self*gearRatio* motorEncoderStepsPerRevolution/360
+        return motorPosition
 
+
+#-------------------------------------------------------------------------------------------#
+#Functions
 def updateLocation(lat,lon):
     """
         Update the observer location
@@ -105,28 +102,27 @@ def print_HaDec(CB,name=''):
                 name : Name of the celestial body (string)
         Outputs: None
     """
-
     observerInfo = observer.at(currentTime).observe(CB).apparent().hadec() 
     print('\nCurrent position of', name)
     print('  HA :', observerInfo[0])
     print('  Dec:', observerInfo[1])
     print('  Distance:', observerInfo[2])
+    
+    return  observerInfo[0].degrees, observerInfo[1].degrees #Ha,dec
 
-    return observerInfo[0], observerInfo[1] #Ha,dec
-
-
+#-------------------------------------------------------------------------------------------#
+#Main function
 def main():
 
     jupiter = planets['jupiter barycenter']
 
     a=print_HaDec(jupiter,'Jupiter')
-    print("a0",a[0],a[1])
-    print("hello\n")
-    #print_HaDec(polaris,'Polaris')
-
-    
+    print("angle en degree : \n ha : ",a[0], " \n dec : ",a[1])
+   
 
 
-
+#---------------------------#
 if __name__ == '__main__':
     main()
+#-------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------#
